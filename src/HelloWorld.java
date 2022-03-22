04793159f43a918539c706971178297e0b901f98
ReiDasCapivaras <githubunicampheitor@g.unicamp.br>
2022-03-22 01:22:05 -0300

    Fim das atualizações...

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
         System.out.println("Digite seu nome: ");
         String nome = input.nextLine();
+
         System.out.println("Tudo maiúsculo? (S/N) ");
         String decisao = input.nextLine();
+
+
         System.out.println("------------------------------");
         if (decisao.compareTo("s") == 0)
             System.out.println("Olá, " + maiuscula(nome));
         else
             System.out.println("Olá, " + nome);
         System.out.println("------------------------------");
+
+        System.out.println("Acho que o programa acaba aqui... até mais!!");
+
     }
     public static String maiuscula(String s)
     {
